7e78b9b8e9cb1bfdc7b4aef1ed4956de89fd3ad7
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-02-21 20:38:50 -0600

    Working on css for feature one

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,19 +4,38 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
+	const handleCreate = () => {
+		return
+	}
+
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
+		<ul className='nav-bar'>
+			<div className='button-container'>
+				{isLoaded && (
+					<li>
+						<ProfileButton user={sessionUser} />
+					</li>
+				)}
+				<div>
+					{sessionUser && (
+						<button onClick={handleCreate}>
+							New
+						</button>
+					)}
+					<li>
+						<NavLink exact to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/notes">Notes</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/notebooks">Notebooks</NavLink>
+					</li>
+				</div>
+			</div>
 		</ul>
 	);
 }
