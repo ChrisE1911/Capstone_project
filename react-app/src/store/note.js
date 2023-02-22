b6e9fd9233793e3af0610a044756f06bcab808db
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-02-22 12:22:12 -0600

    Added create note case in note store

--- react-app/src/store/note.js
+++ react-app/src/store/note.js
@@ -1,8 +1,14 @@
 const GET_NOTES = "notes/GET_NOTES"
+const CREATE_NOTE = "notes/CREATE_NOTE"
 
 
 const getAllNotesAction = (data) => ({
     type: GET_NOTES,
+    payload: data
+})
+
+const createNoteAction = (data) => ({
+    type: CREATE_NOTE,
     payload: data
 })
 
@@ -18,23 +24,48 @@ export const thunkGetAllNotes = () => async (dispatch) => {
     }
 }
 
+export const thunkCreateNote = (note) => async (dispatch) => {
+    const response = await fetch("/api/notes/new", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ note }),
+    })
+
+    if (response.ok) {
+        const note = await response.json()
+        dispatch(createNoteAction(note))
+        return note
+    }
+
+}
+
 
 
 const initialState = {
-    allNotes:{}
+    allNotes: {},
+    singleNote: {}
 }
 
 
 export default function noteReducer(state = initialState, action) {
     switch (action.type) {
         case GET_NOTES:
-            const newState = { ...state }
+            let newState;
+            newState = { ...state }
             const newNotes = {}
             action.payload.forEach((note) => {
                 newNotes[note.id] = note
             })
             newState.allNotes = newNotes
             return newState
+        case CREATE_NOTE:
+            newState = { ...state }
+            const updatedNotes = { ...state.allNotes, [action.payload.id]: action.payload }
+            newState.allNotes = updatedNotes
+            return newState
+
         default:
             return state
     }
