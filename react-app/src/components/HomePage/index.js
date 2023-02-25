044bb09309edb0f47ecab1772d3c1b323aa1a6a1
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-02-25 16:38:04 -0600

    Started on home page notes CSS

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,6 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { thunkGetAllNotes } from '../../store/note'
+import { useHistory, Link } from 'react-router-dom'
+import "./HomePage.css"
+
 function HomePage() {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const all_notes = useSelector((state) => state.noteReducer.allNotes)
+    const all_notes_arr = Object.values(all_notes)
+
+    console.log(all_notes_arr)
+
+    useEffect(() => {
+        dispatch(thunkGetAllNotes())
+    }, [dispatch])
+
+    if (all_notes_arr.length === 0) return null;
     return (
-        <h1>Hello World. How are you?</h1>
+        <>
+            <ul className='notes-container'>
+                <h1>Notes</h1>
+                <div className='note-card-container'>
+                {all_notes_arr.map((note) => (
+                    <Link to={`/notes/${note.id}`} className='one_note' key={note.id}>
+                        <div id='notes-content'>
+                            <div>{note.note_title}</div>
+                            <div>{`${note.note_content?.slice(0, 40)}...`}</div>
+                        </div>
+                    </Link>
+                ))}
+                </div>
+            </ul>
+        </>
     )
 }
 
