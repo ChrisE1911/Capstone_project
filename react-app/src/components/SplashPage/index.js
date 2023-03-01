51e0cb1d673ad27fcaf2591e130d8cbe465e0a0c
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-03-01 12:08:02 -0600

    Splash Page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,11 +1,25 @@
 import './Splashpage.css'
+import LoginFormModal from "../LoginFormModal";
+import OpenModalButton from '../OpenModalButton';
 
 function SplashPage() {
+
+    const handleSubmit = () => {
+       console.log('HERE I AM')
+    }
+
+
     return (
         <>
             <div className="splash-container">
-                <div>Splash Page under Construction...</div>
-                <div>Click on the button in the top left corner to log in as a Demo User and navigate through our site</div>
+                <h1 id='splash-page-h1'>ETERNAL NOTE</h1>
+                <h4>Where your thoughts are endless...</h4>
+                <div>
+                </div>
+                    <h3>Click the button in the top left corner to Login/Sign up and see what our site has to offer...</h3>
+                <button id='splashpage-button'>
+                    <img src='https://luckyretail.com/Uploadfile/201909162/292816/292816-2.jpg'></img>
+                </button>
             </div>
         </>
     )
