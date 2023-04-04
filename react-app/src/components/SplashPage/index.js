b982a038291f8a2ad56723a2ad7d2ec59e154238
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-04-04 09:30:45 -0500

    Modified Splash Page with updated CSS

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,16 +1,31 @@
 import './Splashpage.css'
+import ProfileButton from '../Navigation/ProfileButton'
+import { useSelector } from 'react-redux'
 
-function SplashPage() {
+function SplashPage({ isLoaded }) {
+
+    const sessionUser = useSelector(state => state.session.user);
 
     return (
         <>
+            <div className='splash-top-bar'>
+                <div id='splash-top-bar-title-logo'>
+                    <div id='e-circle-logo'>
+                        <i class="fa-solid fa-e"></i>
+                    </div>
+                    <h1 id='splash-page-h1'>ETERNAL NOTE</h1>
+                    {isLoaded && (
+                        <ProfileButton user={sessionUser} />
+                    )}
+                </div>
+
+            </div>
             <div className="splash-container">
                 <div id='up'></div>
                 <div id='down'></div>
                 <div id='left'></div>
                 <div id='right'></div>
                 <div className='splash-container-content'>
-                    <h1 id='splash-page-h1'>ETERNAL NOTE</h1>
                     <h4>Where your thoughts are endless...</h4>
                     <div>
                     </div>
