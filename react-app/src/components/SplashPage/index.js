fa46cacd544dbc22f87a1c4be340451632ecb514
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-04-04 20:06:15 -0500

    Splash page styling

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -5,7 +5,7 @@ import { logout } from "../../store/session";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { useSelector } from 'react-redux'
 import './Splashpage.css'
 
@@ -45,21 +45,22 @@ function SplashPage({ isLoaded }) {
         <>
             <div className='splash-top-bar'>
                 <div id='splash-top-bar-title-logo'>
-                    <div id='e-circle-logo'>
-                        <i class="fa-solid fa-e"></i>
+                    <div id='logo-and-title'>
+                        <div id='e-circle-logo'>
+                            <i class="fa-solid fa-e"></i>
+                        </div>
+                        <h1 id='splash-page-h1'>ETERNAL NOTE</h1>
                     </div>
-                    <h1 id='splash-page-h1'>ETERNAL NOTE</h1>
+                    <div>
+                        <Link>Portfolio Site</Link>
+                        <Link>Github</Link>
+                        <Link>LinkedIn</Link>
+                    </div>
                     <div id='splash-page-buttons'>
                         <OpenModalButton
                             buttonText="Log In"
                             onItemClick={closeMenu}
                             modalComponent={<LoginFormModal />}
-                        />
-
-                        <OpenModalButton
-                            buttonText="Sign Up"
-                            onItemClick={closeMenu}
-                            modalComponent={<SignupFormModal />}
                         />
                     </div>
                 </div>
@@ -68,6 +69,22 @@ function SplashPage({ isLoaded }) {
             <div className="splash-container">
                 <div className='splash-container-content'>
                     <h1>Where your thoughts are endless...</h1>
+                    <h2>Thoughts are the key to unlocking endless possibilities.</h2>
+                    <div id='account-already-made'>
+                        <OpenModalButton
+                            buttonText="Already have an account? Log in"
+                            onItemClick={closeMenu}
+                            modalComponent={<LoginFormModal />}
+                        />
+                    </div>
+                    <br />
+                    <div id='splash-page-content-buttons'>
+                        <OpenModalButton
+                            buttonText="Sign Up"
+                            onItemClick={closeMenu}
+                            modalComponent={<SignupFormModal />}
+                        />
+                    </div>
                     <button id='splashpage-button'>
                         <img src='https://luckyretail.com/Uploadfile/201909162/292816/292816-2.jpg'></img>
                     </button>
