9e93b0a988d22f011cce9f7d782e60efb41d9e5c
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-04-06 16:20:53 -0500

    added buttons to task list that toggle between completed tasks and uncompleted tasks

--- react-app/src/components/Tasks/index.js
+++ react-app/src/components/Tasks/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { thunkGetAllTasks } from '../../store/task'
 import { thunkUpdateTasks } from '../../store/task'
 import "./Tasks.css"
@@ -8,6 +8,7 @@ function Tasks() {
     const dispatch = useDispatch()
     const tasks = useSelector(state => state.taskReducer.allTasks)
     const tasks_arr = Object.values(tasks)
+    const [showCompleted, setShowCompleted] = useState(false)
 
     console.log(tasks_arr)
 
@@ -20,12 +21,18 @@ function Tasks() {
         await dispatch(thunkUpdateTasks(taskId, task))
     }
 
+    const filteredTasks = tasks_arr.filter(task => task.is_completed === showCompleted)
+
     return (
         <div className="notebooks-container">
             <h1>Tasks List</h1>
             <div className='task-list'>
+                <div>
+                    <button onClick={() => setShowCompleted(true)}>Completed Tasks</button>
+                    <button onClick={() => setShowCompleted(false)}>Uncompleted Tasks</button>
+                </div>
                 <ul>
-                    {tasks_arr.map((task) => (
+                    {filteredTasks.map((task) => (
                         <li id='task-list-one' key={task.id}>
                             {task.task_content}
                             <input
