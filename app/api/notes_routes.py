c9c0eb3358afafd609408041f0ee0c74f880bf19
Christopher Eatmon <eatmonchristopher1@gmail.com>
2023-07-24 14:14:40 -0500

    Chore: Remove Print Statement

--- app/api/notes_routes.py
+++ app/api/notes_routes.py
@@ -20,14 +20,14 @@ def validation_errors_to_error_messages(validation_errors):
 @login_required
 def get_all_notes():
     """Returns all notes made by that current user"""
-    # print('YOU HIT THE ROUTE!!!')
+
     my_id = current_user.to_dict()
-    # print('BBBBBBBB', my_id['id'])
+
     all_notes = Note.query.filter(Note.user_id == my_id['id']).all()
     all_notes_arr = [note.to_dict() for note in all_notes]
-    # print('CCCCCCCC', all_notes_arr)
 
-    # print('AAAAAAAAAAAA', all_notes)
+
+
     return all_notes_arr
 
 @notes_routes.route('/<int:id>')
@@ -35,7 +35,7 @@ def get_all_notes():
 def get_one_note(id):
     """Returns the note queried that matches the id in the params"""
     note = Note.query.get(id)
-    print('NOTEEEEEEE', note)
+
     return note.to_dict()
 
 
@@ -43,17 +43,17 @@ def get_one_note(id):
 @login_required
 def create_note():
     """Creates note"""
-    print("IM IN THE POST ROUTE")
+
     form = NoteForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    # print('FORMMMMMMMMMMMM', form.data)
+
     if form.validate_on_submit():
         note = Note(
             user_id=current_user.id,
             note_title=form.data['note_title'],
             note_content=form.data['note_content'],
         )
-        print('NEW NOTEEEEE', note)
+
         db.session.add(note)
         db.session.commit()
         return note.to_dict()
@@ -92,11 +92,8 @@ def delete_note(id):
 @notes_routes.route('/<int:noteId>/notebooks/<int:notebookId>/add-note', methods=['PUT'])
 @login_required
 def add_note_to_notebook(noteId, notebookId):
-    print('NOTEID', noteId)
-    print('NOTEBOOKID', notebookId)
 
     to_update_note = Note.query.get(noteId)
-    print('TO UPDATE NOTE', to_update_note.to_dict())
 
     to_update_note.notebook_id = notebookId
 
@@ -107,11 +104,8 @@ def add_note_to_notebook(noteId, notebookId):
 @notes_routes.route('/<int:noteId>/notebooks/<int:notebookId>/edit', methods=['PUT'])
 @login_required
 def edit_note_to_notebook(noteId, notebookId):
-    print('NOTEID', noteId)
-    print('NOTEBOOKID', notebookId)
 
     to_update_note = Note.query.get(noteId)
-    print('TO UPDATE NOTE', to_update_note.to_dict())
 
     to_update_note.notebook_id = notebookId
 
@@ -122,10 +116,8 @@ def edit_note_to_notebook(noteId, notebookId):
 @notes_routes.route('/<int:noteId>/delete', methods=['PUT'])
 @login_required
 def delete_note_to_notebook(noteId):
-    print('NOTEID', noteId)
 
     to_update_note = Note.query.get(noteId)
-    print('TO UPDATE NOTE', to_update_note.to_dict())
 
     to_update_note.notebook_id = None
 
